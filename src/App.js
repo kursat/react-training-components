ca8cba67362e0d3bf49c754afa99169a93416062
Kürşat Yiğitoğlu <kursat.yigitoglu@gmail.com>
2024-02-20 11:41:31 +0300

    first component and props intro

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import { Button } from '@nextui-org/react';
+import UserItem from './UserItem';
+import data from './data.json';
 
 function App() {
+    const user1 = {
+        id: '6deb7521cae6e3b247f156cf',
+        sex: 'female',
+        firstName: 'Zehra',
+        lastName: 'Karaböcek',
+        fullName: 'Zehra Karaböcek',
+        email: 'zehra61@goc.gov.tr',
+    };
+    const user2 = {
+        id: 'fa05bee6adae4c7ffe2e964c',
+        sex: 'male',
+        firstName: 'Afi',
+        lastName: 'Sepetçi',
+        fullName: 'Afi Sepetçi',
+        email: 'afi81@goc.gov.tr',
+    };
+
     return (
         <div className="App">
-            <Button variant={'shadow'} color={'secondary'}>
-                Test
-            </Button>
+            <ul>
+                <UserItem user={user1} />
+                <UserItem user={user2} />
+            </ul>
         </div>
     );
 }
