8a3a5059fc9609641fa6b638c88be7359b893f15
Kürşat Yiğitoğlu <kursat.yigitoglu@gmail.com>
2024-02-21 09:54:10 +0300

    css styles

--- src/App.js
+++ src/App.js
@@ -1,52 +1,10 @@
-import UserItem from './UserItem';
-import data from './data.json';
-import MyButton from './ui/Button';
-import { Button } from '@nextui-org/react';
+import Button from './ui/Button';
 
 function App() {
-    const user1 = {
-        id: '6deb7521cae6e3b247f156cf',
-        sex: 'female',
-        firstName: 'Zehra',
-        lastName: 'Karaböcek',
-        fullName: 'Zehra Karaböcek',
-        email: 'zehra61@goc.gov.tr',
-    };
-    const user2 = {
-        id: 'fa05bee6adae4c7ffe2e964c',
-        sex: 'male',
-        firstName: 'Afi',
-        lastName: 'Sepetçi',
-        fullName: 'Afi Sepetçi',
-        email: 'afi81@goc.gov.tr',
-    };
-
     return (
         <div className="App">
-            <ul>
-                <UserItem user={user1} />
-                <UserItem user={user2} />
-            </ul>
-
-            <MyButton
-                text={'My Text'}
-                customClass={'my-class'}
-                onPress={() => {
-                    alert('Button 1');
-                }}
-            />
-            <MyButton
-                text={'My Text 2'}
-                onPress={() => {
-                    alert('Button 2');
-                }}
-            />
-            <Button color={'secondary'} size={'lg'}>
-                NextUI Button
-            </Button>
-            <Button radius={'none'} color={'secondary'} size={'sm'}>
-                NextUI Button
-            </Button>
+            <div>Test</div>
+            <Button />
         </div>
     );
 }
--- src/ui/Button.js
+++ src/ui/Button.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../custom.css';
 
 const Button = ({ text, customClass, onPress }) => {
     const classes = `${customClass} border-2 border-blue-800 px-2 bg-blue-600 rounded text-white`;
