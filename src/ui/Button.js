d04849b10569b8ee4bae73938c30cf6a137db37d
Kürşat Yiğitoğlu <kursat.yigitoglu@gmail.com>
2024-02-21 10:12:13 +0300

    scss

--- src/ui/Button.js
+++ src/ui/Button.js
@@ -4,7 +4,7 @@ import style from './Button.module.css';
 const Button = ({ text, customClass, onPress }) => {
     console.log('style', style);
 
-    const classes = `${style.container} ${customClass} border-2 border-blue-800 px-2 bg-blue-600 rounded text-white`;
+    const classes = `mt-45 ${style.container} ${customClass} border-2 border-blue-800 px-2 bg-blue-600 rounded text-white`;
 
     return (
         <button onClick={onPress} className={classes}>
